76e550831e538f2613a740f40fc2d840ee45c3ad
Luke1760 <kjd1760@gmail.com>
2021-12-12 09:54:03 +0800

    state: simple user create test from Minitest

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  # success case
+  # test 'valid: User with all valid things' do
+  #   user = User.new(email: "user0000@test.com", password_digest: "123qwe", role: 1)
+  #   assert user.valid?
+  # end
+
+  # fail case1
+  # invalid email, valid password, valid role
+  # test 'invalid: email is not valid' do
+  #   user = User.new(email: "user01", password_digest: "123qwe", role: 1)
+  #   assert user.valid?
+  # end
+
+  # fail case2
+  # taken email, valid password, valid role
+  # test 'invalid: email has been taken' do
+  #   user = User.new(email: users(:one).email, password_digest: "123qwe", role: 1)
+  #   assert user.valid?
+  # end
+
+  # fail case3
+  # valid email, invalid password, valid role
+  # test 'invalid: password cannot be blank' do
+  #   user = User.new(email: users(:one).email, password_digest: "", role: 1)
+  #   assert user.valid?
+  # end
+
+   # fail case4
+  # valid email, valid password, invalid role
+  # test 'invalid: role can be only in [0 1 2]' do
+  #   user = User.new(email: users(:one).email, password_digest: "123qwe", role: 5)
+  #   assert user.valid?
   # end
 end
